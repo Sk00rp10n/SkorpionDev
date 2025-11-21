5f31fe1d5928ec40b345404fc068463b7cacb99c
Ciber.HENRY <150444564+Sk00rp10n@users.noreply.github.com>
2025-11-21 13:58:01 -0300

    Delete risco de loop infinito.py

--- risco de loop infinito.py
+++
@@ -1,5 +0,0 @@
-while i < len(pedidos) and pedidos[i]["status"] == "pago":
-    total += pedidos[i]["valor"]
-    if not pedidos[i]["cancelado"]:
-        validos += 1
-    # (intencionalmente sem i += 1)
